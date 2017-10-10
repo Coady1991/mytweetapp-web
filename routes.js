c6a825b64467f1732791e393f957dfa48e1f7540
Niall Coady <niall_coady7@hotmail.com>
2017-10-10 15:06:00 +0100

    Introduce and serve static assets

--- routes.js
+++ routes.js
@@ -1,5 +1,13 @@
 const Tweets = require('./app/controllers/tweets');
+const Assets = require('./app/controllers/assets');
 
 module.exports = [
   { method: 'GET', path: '/', config: Tweets.home },
+
+  {
+    method: 'GET',
+    path: '/{param*}',
+    config: { auth: false },
+    handler: Assets.servePublicDirectory,
+  },
 ];
