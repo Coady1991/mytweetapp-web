dfdbd9c4963a0611d5e8ae6be8437ee66f1cf990
Niall Coady <niall_coady7@hotmail.com>
2017-10-10 18:51:15 +0100

    Begin to implement Handlebar expressions

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -2,18 +2,18 @@
 
 exports.home = {
   handler: (request, reply) => {
-    reply.file('./app/views/main.html');
+    reply.view('main', { title: 'Welcome to MyTweet' });
   },
 };
 
 exports.signup = {
   handler: (request, reply) => {
-    reply.file('./app/views/signup.html');
+    reply.view('signup', { title: 'Signup for MyTweet' });
   },
 };
 
 exports.login = {
   handler: (request, reply) => {
-    reply.file('./app/views/login.html');
+    reply.view('login', { title: 'Login to MyTweet' });
   },
 };
