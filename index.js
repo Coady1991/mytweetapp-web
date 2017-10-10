5e59f38a72a177dd8bd4ddc1934c8f88fa08d57f
Niall Coady <niall_coady7@hotmail.com>
2017-10-10 21:20:12 +0100

    Redirect from protected routes to login page

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ server.register([require('inert'), require('vision'), require('hapi-auth-cookie'
     cookie: 'tweet-cookie',
     isSecure: false,
     ttl: 24 * 60 * 60 * 1000,
+    redirectTo: '/login',
   });
 
   server.auth.default({
