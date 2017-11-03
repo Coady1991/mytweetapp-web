a39b881b4103612182c95aeffa205e9b3ecc3e16
Niall Coady <niall_coady7@hotmail.com>
2017-11-03 14:36:45 +0000

    Implemented Boom dependency, userapi & routesapi to find users

---
+++ app/api/usersapi.js
@@ -0,0 +1,17 @@
+'use srict'
+
+const User = require('../models/user');
+const Boom = require('boom');
+
+exports.find = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    User.find({}).exec().then(users => {
+      reply(users);
+    }).catch(err => {
+      reply(Boom.badImplementation('Error accessing db'));
+    });
+  },
+};
---
+++ routesapi.js
@@ -0,0 +1,6 @@
+const UsersApi = require('./app/api/usersapi');
+
+module.exports = [
+  { method: 'GET', path: '/api/users', config: UsersApi.find },
+
+];
