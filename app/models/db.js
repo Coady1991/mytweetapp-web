204c80f67051f447d49ecbbfdc3ad7ff6abf29bb
Niall Coady <niall_coady7@hotmail.com>
2017-11-05 14:07:41 +0000

    Implemented data seeder

--- app/models/db.js
+++ app/models/db.js
@@ -21,3 +21,19 @@ mongoose.connection.on('error', function (err) {
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
 });
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV != 'production') {
+    var seeder = require('mongoose-seeder');
+    const data = require('./data.json');
+    const Tweet = require('./tweet');
+    const User = require('./user');
+    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
+    }).catch(err => {
+      console.log(error);
+    });
+  }
+});
